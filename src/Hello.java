0e03e5b644c006fab3475ec26a8bdb84108d7d84
evangelis <angelisevang@gmail.com>
2020-06-04 00:32:02 +0300

    Thrid Commit

--- src/Hello.java
+++ src/Hello.java
@@ -4,6 +4,7 @@ public class Hello{
 	public static void main(String[] args) {
 		System.out.println("Hello World from GIT");
 		System.out.println("Changes after first commit");
+		System.out.println("Adding line after pushing to origin");
 	}
 }
 
